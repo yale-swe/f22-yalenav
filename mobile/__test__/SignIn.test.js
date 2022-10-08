3cb51f849669484c3901ea87e7315ae58b501baf
Jonathan Yu <jonyu02@gmail.com>
2022-10-08 19:09:59 -0400

    added verification that toClick exists in jest tests

--- mobile/__test__/SignIn.test.js
+++ mobile/__test__/SignIn.test.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react-native';
+import {
+    render,
+    screen,
+    fireEvent
+} from '@testing-library/react-native';
 import Navigation from '../src/navigation';
 
-describe('Testing Navigation to Sign Up Screen', () => {  
+describe('Testing Navigation to Sign Up Screen', () => {
     test('clicking on one item takes you to the details screen', async () => {
-    const component = (
-        <Navigation />
-    );
+        const component = ( <
+            Navigation / >
+        );
 
-    render(component);
-    const toClick = await screen.findByText(`Don't have an account? Sign up!`);
+        render(component);
+        const toClick = await screen.findByText(`Don't have an account? Sign up!`);
 
-    fireEvent(toClick, 'press');
-    const newHeader = await screen.findByText('Create an Account!');
+        expect(toClick).toBeTruthy();
 
-    expect(newHeader).toBeTruthy();
+        fireEvent(toClick, 'press');
+        const newHeader = await screen.findByText('Create an Account!');
+
+        expect(newHeader).toBeTruthy();
     });
 });
