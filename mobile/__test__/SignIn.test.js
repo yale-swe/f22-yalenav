64ef9063e92b7a4f31b1b427b2c3d5f7cf3055f4
Jonathan Yu <jonyu02@gmail.com>
2022-10-08 23:36:21 -0400

    added testing for forgot password navigation to and from sign in page

--- mobile/__test__/SignIn.test.js
+++ mobile/__test__/SignIn.test.js
@@ -5,6 +5,7 @@ import {
     fireEvent
 } from '@testing-library/react-native';
 import Navigation from '../src/navigation';
+import ForgotPasswordScreen from '../src/screens/ForgotPasswordScreeen';
 
 describe('Testing Navigation to Sign Up Screen', () => {
     test('clicking on one item takes you to the details screen', async () => {
@@ -22,4 +23,42 @@ describe('Testing Navigation to Sign Up Screen', () => {
 
         expect(newHeader).toBeTruthy();
     });
+});
+
+describe("Testing 'Forgot password' navigation", () => {
+    test("clicking 'Forgot password?' button navigates to 'Forgot your password?' screen", async () => {
+        const component = ( <
+            Navigation / >
+        );
+
+        render(component);
+        const forgotButton = await screen.findByText('Forgot Password?');
+
+        expect(forgotButton).toBeTruthy();
+
+        fireEvent(forgotButton, 'press');
+        const newHeader = await screen.findByText('Forgot your password?');
+
+        expect(newHeader).toBeTruthy();
+    });
+
+    test("clicking 'Back to Sign In' button navigates to Sign In screen", async () => {
+        const component = ( <
+            Navigation /
+            >
+        );
+
+        render(component);
+        const forgotButton = await screen.findByText('Forgot Password?');
+        fireEvent(forgotButton, 'press')
+
+        const backToSignInButton = await screen.findByText('Back to Sign In');
+
+        expect(backToSignInButton).toBeTruthy();
+
+        fireEvent(backToSignInButton, 'press');
+        const signInButton = await screen.findByText('Sign In');
+
+        expect(signInButton).toBeTruthy();
+    })
 });
