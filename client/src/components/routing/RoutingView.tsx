3d5bb0a28440b6e07dd51d716fea69d79e444a9e
earth418 <alihafez42@gmail.com>
2022-10-31 14:11:09 -0400

    skeleton code for mode switching

--- client/src/components/routing/RoutingView.tsx
+++ client/src/components/routing/RoutingView.tsx
@@ -1,13 +1,26 @@
 import React from "react";
-import MapViewDirections from "react-native-maps-directions";
+import MapViewDirections, { MapViewDirectionsMode } from "react-native-maps-directions";
 import { Location } from "../../../types";
 import { APIKEY, YALE_HEX } from "../../constants";
+
+export const enum RoutingMode {
+  walking,
+  biking,
+  shuttle
+};
+
+function getClosestShuttleStop(x : Location) {
+  
+
+  
+  return {};
+};
 
 // routes between locations using specified mode
 interface RoutingInterface {
     routeOrigin: Location;
     routeDestination : Location;
-    mode?: String | undefined;
+    mode?: RoutingMode | undefined;
     resultHandler?: Function | undefined;
   }
   
@@ -18,6 +31,26 @@ interface RoutingInterface {
      
     // UI customizability can be added in here; whether you want
     // the lines to be thicker, a certain color, etc
+    
+    // TODO: implement mode switching
+    // Include bycicles, walking, and custom mode for routing.
+    ///////////////////////////////////////////////////////////
+    /*
+    let m = "";
+    switch (mode) {
+      case RoutingMode.walking:
+        m = "WALKING";
+        break;
+      case RoutingMode.biking:
+        m = "BIKING";
+        break;
+      case RoutingMode.shuttle:
+        m = "WALKING";
+        routeDestination = getClosestShuttleStop(origin);
+        break;
+    }*/
+    ////////////////////////////////////////////////////////////
+
 
     return (
       <MapViewDirections 
@@ -26,6 +59,7 @@ interface RoutingInterface {
         apikey={APIKEY}
         strokeColor={YALE_HEX}
         strokeWidth={4}
+        mode={"WALKING"}
         onReady = {result => {
           resultHandler && resultHandler(result.duration, result.distance);
         }}
