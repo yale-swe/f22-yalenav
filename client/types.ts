6bcdba9891d482e8e691e9cc900729d072ad837b
Kyle Zawadzki <kylezawadzki@Kyles-MacBook-Pro.local>
2022-11-07 14:40:14 -0500

    Fixed the conflicts between the routing and banner merge. Some features were using the new Location type but the return from the database on the inital query does not have a loc field but instead uses lat and lon. Returned to using lat and lon on building types for that reason for now

--- client/types.ts
+++ client/types.ts
@@ -1,20 +1,22 @@
 export type Location = {
   latitude: number;
   longitude: number;
-}
+};
 
 export type Building = {
   _id: string;
   name: string;
   address: string;
   abbreviation: string;
-  loc : Location;
+  lat: number;
+  lon: number;
 };
 
 export type ShuttleStop = {
   _id: number;
   name: string;
-  loc : Location;
+  lat: number;
+  lon: number;
 };
 
 export type AuthData = {
