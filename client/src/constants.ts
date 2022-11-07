17378a7e8fa0e2b8e0d309395db745c8f48f58e3
Kyle Zawadzki <kylezawadzki@Kyles-MacBook-Pro.local>
2022-11-07 13:25:22 -0500

    changed IP

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,3 +1,3 @@
 export const YALE_HEX = "#00356B";
-export const BACKEND = "http://172.26.74.131:4000";
-export const APIKEY = 'AIzaSyAaFzpWVbgNHe41BXKM04MGOT6Vf_y7tn0';
+export const BACKEND = "http://172.26.52.247:4000";
+export const APIKEY = "AIzaSyAaFzpWVbgNHe41BXKM04MGOT6Vf_y7tn0";
