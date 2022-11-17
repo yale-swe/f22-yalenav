8d6e88588b2e626370f31da48a53511a6ae8eb44
Vincent Schaffer <vincent.schaffer@yale.edu>
2022-11-17 11:51:36 -0500

    Post merge fixes

--- client/src/navigation/Navigation.tsx
+++ client/src/navigation/Navigation.tsx
@@ -1,33 +1,33 @@
-import {Keyboard, StyleSheet, TouchableWithoutFeedback} from "react-native";
-import {useAuth} from "../contexts/Auth";
+import { Keyboard, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import { useAuth } from "../contexts/Auth";
 import AuthStack from "./AuthStack";
 import UnauthStack from "./UnauthStack";
-import {NavigationContainer} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export type RootStackParamList = {
-    Home: undefined;
-    EditSchedule: undefined;
-    UserProfile: undefined;
-    SignIn: undefined;
+  Home: undefined;
+  EditSchedule: undefined;
+  UserProfile: undefined;
+  SignIn: undefined;
 };
 
-const Navigation = () => {
-    const auth = useAuth();
-    return (
-        <NavigationContainer>
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                {auth.authData ? <AuthStack/> : <UnauthStack/>}
-            </TouchableWithoutFeedback>
-        </NavigationContainer>
-    );
+export const Navigation = () => {
+  const auth = useAuth();
+  return (
+    <NavigationContainer>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        {auth.authData ? <AuthStack /> : <UnauthStack />}
+      </TouchableWithoutFeedback>
+    </NavigationContainer>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        backgroundColor: "#fff",
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
 });
 
 export default Navigation;
