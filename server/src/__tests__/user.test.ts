3850ddd20a99d7a92af527073d785a401f1721e7
Vincent Schaffer <vincent.schaffer@yale.edu>
2022-11-19 18:54:11 -0500

    Added backend testing

--- server/src/__tests__/user.test.ts
+++ server/src/__tests__/user.test.ts
@@ -1,11 +1,13 @@
 import request from "supertest";
 import app from "../app";
-import { connectMongoose, disconnectMongoose } from "../testUtils/mongoose";
 import { User } from "../models";
+import { connectMongoose, disconnectMongoose } from "../testUtils/mongoose";
 
 describe("User Tests", () => {
   let testUsers: typeof User[] = [];
   let netid: String = "vs399";
+  let valid_course_code: String = "CPSC 439";
+  let invalid_course_code: String = "CPSC 666";
 
   beforeEach(async () => {
     await connectMongoose();
@@ -22,6 +24,43 @@ describe("User Tests", () => {
       const res = await request(app).get(`/user?netid=${netid}`);
       expect(res.status).toEqual(200);
       expect(res.body.user.first_name).toEqual("Vincent");
+    });
+  });
+
+  describe("Edit User Schedule", () => {
+    it("should add the valid course to their schedule", async () => {
+      // fetch user
+      const res = await request(app).post(
+        `/user?netid=${netid}&course_code=${valid_course_code}`
+      );
+      expect(res.status).toEqual(201);
+      expect(res.body.user.courses).toHaveLength(1);
+    });
+
+    it("should not add invalid course to their schedule", async () => {
+      // fetch user
+      const res = await request(app).post(
+        `/user?netid=${netid}&course_code=${invalid_course_code}`
+      );
+      expect(res.status).toEqual(400);
+    });
+
+    it("should not add duplicate course to their schedule", async () => {
+      // fetch user
+      const res = await request(app).post(
+        `/user?netid=${netid}&course_code=${valid_course_code}`
+      );
+      expect(res.status).toEqual(400);
+    });
+
+    it("should delete course from their schedule", async () => {
+      // fetch user
+      const res = await request(app).delete(
+        `/user?netid=${netid}&course_code=${valid_course_code}`
+      );
+      expect(res.status).toEqual(201);
+      // same length
+      expect(res.body.user.courses).toHaveLength(0);
     });
   });
 });
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -29,7 +29,28 @@ router.post("/", async (req: Request, res: Response) => {
   const addedCourse = await Course.findOne({
     course_code: course_code.toString(),
   });
+
+  // invalid course
+  if (!addedCourse)
+    return res
+      .status(400)
+      .send({ message: `Course with code ${course_code} does not exist.` });
+
+  const existingUser = await User.findOne({ netid: netid });
+
+  // duplicate course
+  if (
+    existingUser.courses.some((c) => {
+      return c.course_code === addedCourse.course_code;
+    })
+  ) {
+    return res
+      .status(400)
+      .send({ message: `Already have ${course_code} in your schedule.` });
+  }
+
   const update = { $push: { courses: addedCourse } };
+
   const oldUser = await User.findOneAndUpdate(filter, update);
   const user = await User.findOne({ netid: oldUser.netid });
   res.status(201).send({ user });
