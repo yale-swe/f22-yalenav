21d9abdccfc19a07492313c4dd512996abd17718
krz4420 <43185096+krz4420@users.noreply.github.com>
2022-11-27 17:32:59 -0500

    Frontend testing for the searchBar component and the map banner component. Added data mocks for buildings and results. (#27)
    Co-authored-by: Kyle Zawadzki <kylezawadzki@Kyles-MBP.home>

---
+++ client/__test__/mockData/buildingMock.ts
@@ -0,0 +1,247 @@
+import { Building, LatLng } from "../../types";
+
+const mockCoords: LatLng = { latitude: 40, longitude: -72 };
+
+export const mockWatson: Building = {
+  _id: "636af735cd21662437d3e79d",
+  abbreviation: "Wts",
+  address: "Sachem Street, 60, New Haven, Ct, 06511",
+  coords: {
+    latitude: 41.315756,
+    longitude: -72.92372,
+  },
+  name: "Watson Center",
+  tile: [
+    {
+      latitude: 41.315903124652614,
+      longitude: -72.92392248709803,
+    },
+    {
+      latitude: 41.31583629408398,
+      longitude: -72.92358487844469,
+    },
+    {
+      latitude: 41.31579499563062,
+      longitude: -72.92359828948976,
+    },
+    {
+      latitude: 41.315784604123536,
+      longitude: -72.9235437213356,
+    },
+    {
+      latitude: 41.31587457067593,
+      longitude: -72.92351245880128,
+    },
+    {
+      latitude: 41.31584838143122,
+      longitude: -72.92337566614152,
+    },
+    {
+      latitude: 41.31564909582726,
+      longitude: -72.92345136389486,
+    },
+    {
+      latitude: 41.315568069586114,
+      longitude: -72.92347837850684,
+    },
+    {
+      latitude: 41.31550187658784,
+      longitude: -72.92350307106973,
+    },
+    {
+      latitude: 41.31549775990376,
+      longitude: -72.9234913927212,
+    },
+    {
+      latitude: 41.31541424280363,
+      longitude: -72.92352452874185,
+    },
+    {
+      latitude: 41.31541827194572,
+      longitude: -72.92353928089143,
+    },
+    {
+      latitude: 41.31536690036543,
+      longitude: -72.92355939745903,
+    },
+    {
+      latitude: 41.31540114809012,
+      longitude: -72.92373508214952,
+    },
+    {
+      latitude: 41.31564217814121,
+      longitude: -72.92365225257528,
+    },
+    {
+      latitude: 41.31570402365469,
+      longitude: -72.92399565194502,
+    },
+  ],
+};
+
+export const hhMock: Building = {
+  _id: "636af734cd21662437d3cc4f",
+  abbreviation: "Hlh46",
+  address: "Hillhouse Avenue, 46, New Haven, Ct, 06511",
+  coords: {
+    latitude: 41.315049,
+    longitude: -72.923594,
+  },
+  name: "Hillhouse Ave,46",
+  tile: [
+    {
+      latitude: 41.315116085598014,
+      longitude: -72.9237598925829,
+    },
+    {
+      latitude: 41.315189617738255,
+      longitude: -72.92373374104501,
+    },
+    {
+      latitude: 41.31513371317345,
+      longitude: -72.92345143854618,
+    },
+    {
+      latitude: 41.315091910629675,
+      longitude: -72.92346686124803,
+    },
+    {
+      latitude: 41.3150803267875,
+      longitude: -72.92341388761999,
+    },
+    {
+      latitude: 41.314997225250714,
+      longitude: -72.92344406247139,
+    },
+    {
+      latitude: 41.315007801815824,
+      longitude: -72.92350441217424,
+    },
+    {
+      latitude: 41.31497053200732,
+      longitude: -72.92351782321931,
+    },
+    {
+      latitude: 41.3150072981699,
+      longitude: -72.92368881404401,
+    },
+    {
+      latitude: 41.31509543614646,
+      longitude: -72.92365729808809,
+    },
+  ],
+};
+
+export const roseHouseMock: Building = {
+  _id: "636af734cd21662437d3cc5b",
+  abbreviation: "Rah",
+  address: "York Street, 232, New Haven, Ct, 06511",
+  coords: {
+    latitude: 41.309894,
+    longitude: -72.931056,
+  },
+  name: "Rose Alumni House",
+  tile: [
+    {
+      latitude: 41.30994243803472,
+      longitude: -72.93135926127434,
+    },
+    {
+      latitude: 41.31005324865213,
+      longitude: -72.9312774538994,
+    },
+    {
+      latitude: 41.30987393683157,
+      longitude: -72.93084964156151,
+    },
+    {
+      latitude: 41.30983968620302,
+      longitude: -72.9308804869652,
+    },
+    {
+      latitude: 41.30987998105828,
+      longitude: -72.93099448084833,
+    },
+    {
+      latitude: 41.309812487161636,
+      longitude: -72.93104678392412,
+    },
+  ],
+};
+
+export const evansHallMock: Building = {
+  _id: "636af734cd21662437d3cda1",
+  abbreviation: "Evn",
+  address: "Hillhouse Avenue, 56, New Haven, Ct, 06511",
+  coords: {
+    latitude: 41.315689,
+    longitude: -72.923332,
+  },
+  name: "Evans Hall, Thomas M.",
+  tile: [
+    {
+      latitude: 41.31566203457868,
+      longitude: -72.92344942688942,
+    },
+    {
+      latitude: 41.315857950579556,
+      longitude: -72.92337365448475,
+    },
+    {
+      latitude: 41.315853921464644,
+      longitude: -72.92335487902164,
+    },
+    {
+      latitude: 41.315857950579556,
+      longitude: -72.9233528673649,
+    },
+    {
+      latitude: 41.31583629408398,
+      longitude: -72.92325228452684,
+    },
+    {
+      latitude: 41.31583125768864,
+      longitude: -72.92325697839262,
+    },
+    {
+      latitude: 41.315827732211666,
+      longitude: -72.92324021458626,
+    },
+    {
+      latitude: 41.31580406114703,
+      longitude: -72.92324893176557,
+    },
+    {
+      latitude: 41.31579549927046,
+      longitude: -72.9232107102871,
+    },
+    {
+      latitude: 41.31576528087361,
+      longitude: -72.92322210967541,
+    },
+    {
+      latitude: 41.315763266313326,
+      longitude: -72.92320534586906,
+    },
+    {
+      latitude: 41.3156212396561,
+      longitude: -72.92326033115388,
+    },
+    {
+      latitude: 41.315628290632205,
+      longitude: -72.9232891649008,
+    },
+    {
+      latitude: 41.315616203244126,
+      longitude: -72.92329587042332,
+    },
+    {
+      latitude: 41.31564440714614,
+      longitude: -72.92342394590379,
+    },
+    {
+      latitude: 41.31565649452897,
+      longitude: -72.92342126369478,
+    },
+  ],
+};
