53ffd8ccd0c337502398bb4788fbbde2d113bf5a
Kyle Zawadzki <kylezawadzki@Kyles-MacBook-Pro.local>
2022-11-28 13:46:36 -0500

    Fixed SearchBarTest. It was a duplicate of MapBanner.test but now it tests the search bar component.

--- client/__test__/searchBar.test.tsx
+++ client/__test__/searchBar.test.tsx
@@ -1,44 +1,35 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
-import { MapBanner } from "../src/components/map/MapBanner";
+import { Search } from "../src/components/search/Search";
+import { Building } from "../types";
 import { mockWatson } from "./mockData/buildingMock";
-import { mockResultWatson } from "./mockData/resultsMockData";
 
-const mockNavigationHandler = () => {};
-
-const clickDirectionButton = () => {
+const enterQuery = (query: string) => {
   const result = render(
-    <MapBanner
-      selectedLocation={mockWatson}
-      navigationHandler={mockNavigationHandler}
-      results={mockResultWatson}
-    />
+    <Search locations={mockBuildings} selectLocation={mockSelectLocation} />
   );
-
-  const input = result.getByText("Directions");
-  fireEvent.press(input);
+  const input = result.getByPlaceholderText("Search for a Yale Location...");
+  fireEvent.changeText(input, query);
   return result;
 };
 
-describe("Testing the Homepages's Map Banner", () => {
-  test("Clicking Direction Button Displays Time Estimate", async () => {
-    const clickedButton = clickDirectionButton();
-    const duration = Math.ceil(mockResultWatson[0].duration).toString();
+const mockBuildings: Array<Building> = [mockWatson];
 
-    expect(clickedButton.getByText(duration));
-    expect(clickedButton.getByText("min"));
+const mockSelectLocation = () => {};
+describe("Testing the Homepage's Search Bar", () => {
+  test("Text can be entered in the search bar", async () => {
+    const query = "Wts";
+    const enterQueryResult = enterQuery(query);
+    expect(enterQueryResult.getByDisplayValue(query)).toBeDefined();
   });
 
-  test("Clicking Direction Button Displays Steps", async () => {
-    const clickedButton = clickDirectionButton();
-    const distance = mockResultWatson[0].legs
-      ? mockResultWatson[0].legs[0].steps[0].distance.text
-      : null;
+  test("Result is clickable", async () => {
+    const query = "Wts";
+    const enterQueryResult = enterQuery(query);
 
-    const duration = mockResultWatson[0].legs
-      ? mockResultWatson[0].legs[0].steps[0].duration.text
-      : null;
+    fireEvent.press(enterQueryResult.getByText(mockWatson.name));
 
-    expect(clickedButton.getByText(`${duration} ↓ ${distance}`));
+    // Upon clicking the result, the value in the search bar automatically changes to the building's name and not abbreviation.
+    expect(enterQueryResult.getByDisplayValue(mockWatson.name)).toBeDefined();
   });
 });
