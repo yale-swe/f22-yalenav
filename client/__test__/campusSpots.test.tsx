da95a37fc6e75fac468b38661ad75e20bf8db43f
Kyle Zawadzki <kylezawadzki@Kyles-MacBook-Pro.local>
2022-11-30 18:01:49 -0500

    Tests for toggling on and off buttons to ensure all the buildings are rendered when the button is untoggled

--- client/__test__/campusSpots.test.tsx
+++ client/__test__/campusSpots.test.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
 import { CampusSpots } from "../src/components/search/CampusSpots";
 import { colleges, diningHalls, libraries } from "./mockData";
+import { Building } from "../types";
 
-const mockSetBuildings = jest.fn();
 const mockAllBuildings = colleges.concat(diningHalls).concat(libraries);
 
+let mockSetBuildings: any;
 // Render the Campus Spots Component and pass the mockdata as props
 const renderCampusSpots = () => {
   const screen = render(
@@ -19,6 +20,28 @@ const renderCampusSpots = () => {
   );
   return screen;
 };
+
+const untoggleCampusSpotbutton = async (
+  campusSpotName: string,
+  campusSpotMockData: Array<Building>,
+  screen: any
+) => {
+  fireEvent.press(await screen.findByText(campusSpotName));
+  fireEvent.press(await screen.findByText(campusSpotName));
+
+  // SetBuildings function should be called twice
+  expect(mockSetBuildings).toBeCalledTimes(2);
+
+  // First call should be with the campus spots mock data
+  expect(mockSetBuildings).toHaveBeenNthCalledWith(1, campusSpotMockData);
+
+  // Second Call should be with All Buildings
+  expect(mockSetBuildings).toHaveBeenNthCalledWith(2, mockAllBuildings);
+};
+
+beforeEach(() => {
+  mockSetBuildings = jest.fn();
+});
 
 describe("Testing the Homescreen's Campus Spots Component", () => {
   test("User has not clicked any of the campus spots", async () => {
@@ -48,5 +71,23 @@ describe("Testing the Homescreen's Campus Spots Component", () => {
     fireEvent.press(await screen.findByText("Libraries"));
 
     expect(mockSetBuildings).toBeCalledWith(libraries);
+  });
+
+  test("User has clicked on Libraries button twice to untoggle it and show all buildings", async () => {
+    const screen = renderCampusSpots();
+
+    await untoggleCampusSpotbutton("Libraries", libraries, screen);
+  });
+
+  test("User has clicked on Dining Halls button twice to untoggle it and show all buildings", async () => {
+    const screen = renderCampusSpots();
+
+    await untoggleCampusSpotbutton("Dining Halls", diningHalls, screen);
+  });
+
+  test("User has clicked on Colleges button twice to untoggle it and show all buildings", async () => {
+    const screen = renderCampusSpots();
+
+    await untoggleCampusSpotbutton("Colleges", colleges, screen);
   });
 });
