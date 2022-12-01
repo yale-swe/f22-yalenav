36dcc6def67bcd891bac24e9663650a64627d6c0
Kyle Zawadzki <kylezawadzki@Kyles-MacBook-Pro.local>
2022-11-30 21:36:08 -0500

    Abstracted rendering into its own function to reduce repetition

--- client/__test__/profileButton.test.tsx
+++ client/__test__/profileButton.test.tsx
@@ -15,77 +15,51 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
+const renderProfileButton = async (user: any) => {
+  let screen: any;
+  await act(async () => {
+    await waitFor(() => {
+      screen = render(
+        <AuthProvider>
+          <AuthContext.Provider value={user}>
+            <ProfileButton navigation={mockNavigation} route={mockRoute} />
+          </AuthContext.Provider>
+        </AuthProvider>
+      );
+    });
+  });
+  return screen;
+};
+
 describe("Testing the Auth Component", () => {
   test("Logged in user should have their netId in the button", async () => {
-    let screen: any;
-    await act(async () => {
-      await waitFor(() => {
-        screen = render(
-          <AuthProvider>
-            <AuthContext.Provider value={mockLoggedInUser}>
-              <ProfileButton navigation={mockNavigation} route={mockRoute} />
-            </AuthContext.Provider>
-          </AuthProvider>
-        );
-      });
-    });
+    const screen = await renderProfileButton(mockLoggedInUser);
 
     expect(screen.getByText(mockLoggedInUser.authData.netId)).toBeTruthy();
   });
 
   test("Non-logged in should see 'sign in' in the button", async () => {
-    let screen: any;
-    await act(async () => {
-      await waitFor(() => {
-        screen = render(
-          <AuthProvider>
-            <AuthContext.Provider value={mockLoggedOutUser}>
-              <ProfileButton navigation={mockNavigation} route={mockRoute} />
-            </AuthContext.Provider>
-          </AuthProvider>
-        );
-      });
-    });
+    const screen = await renderProfileButton(mockLoggedOutUser);
 
     expect(screen.getByText("Sign In")).toBeTruthy();
   });
 
   test("Logged in user clicks button", async () => {
-    let screen: any;
-    await act(async () => {
-      await waitFor(() => {
-        screen = render(
-          <AuthProvider>
-            <AuthContext.Provider value={mockLoggedInUser}>
-              <ProfileButton navigation={mockNavigation} route={mockRoute} />
-            </AuthContext.Provider>
-          </AuthProvider>
-        );
-      });
-    });
+    const screen = await renderProfileButton(mockLoggedInUser);
 
     const button = screen.getByText(mockLoggedInUser.authData.netId);
     expect(button).toBeTruthy();
+
     fireEvent.press(button);
     expect(mockNavigation.navigate).toHaveBeenCalledWith("UserProfile");
   });
 
   test("Non-logged in user clicks button", async () => {
-    let screen: any;
-    await act(async () => {
-      await waitFor(() => {
-        screen = render(
-          <AuthProvider>
-            <AuthContext.Provider value={mockLoggedOutUser}>
-              <ProfileButton navigation={mockNavigation} route={mockRoute} />
-            </AuthContext.Provider>
-          </AuthProvider>
-        );
-      });
-    });
+    const screen = await renderProfileButton(mockLoggedOutUser);
 
     const button = screen.getByText("Sign In");
     expect(button).toBeTruthy();
+
     fireEvent.press(button);
     expect(mockNavigation.navigate).toHaveBeenCalledWith("SignInScreen");
   });
