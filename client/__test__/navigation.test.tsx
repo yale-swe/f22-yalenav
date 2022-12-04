ed301021af2495ff411e087f2c05247c20f24d4c
Vincent Schaffer <vincent.schaffer@yale.edu>
2022-12-04 16:12:02 -0500

    Add side bar test

--- client/__test__/navigation.test.tsx
+++ client/__test__/navigation.test.tsx
@@ -1,8 +1,11 @@
 import { render } from "@testing-library/react-native";
 import React from "react";
-import { NavigationBar } from "../src/components";
+import { NavigationBar, SideBar } from "../src/components";
+import { mockCoords } from "./mockData";
 
 const mockSelectNextClassHandler = jest.fn();
+const mockCenterOnEvent = jest.fn();
+const mockToggleSwitch = jest.fn();
 
 jest.mock("../src/components/shortcut/NextClass", () => ({
   NextClass: jest.fn(),
@@ -16,6 +19,21 @@ describe("Testing the Homepages's Navigation Bar", () => {
   test("Should be able to render", async () => {
     expect(
       render(<NavigationBar selectNextClass={mockSelectNextClassHandler} />)
+    ).toBeDefined();
+  });
+});
+
+describe("Testing the Homepages's Side Bar", () => {
+  test("Should be able to render", async () => {
+    expect(
+      render(
+        <SideBar
+          origin={mockCoords}
+          centerOnEvent={mockCenterOnEvent}
+          toggleSwitch={mockToggleSwitch}
+          shuttleEnabled={false}
+        />
+      )
     ).toBeDefined();
   });
 });
