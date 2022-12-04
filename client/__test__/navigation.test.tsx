da710f3499fbafd26f3832ec9ec846d2522840c7
Vincent Schaffer <vincent.schaffer@yale.edu>
2022-12-04 01:08:06 -0500

    Mock next class

--- client/__test__/navigation.test.tsx
+++ client/__test__/navigation.test.tsx
@@ -1,17 +1,16 @@
-import { fireEvent, render } from "@testing-library/react-native";
+import { render } from "@testing-library/react-native";
 import React from "react";
 import { NavigationBar } from "../src/components";
 
 const mockSelectNextClassHandler = jest.fn();
 
-const clickDirectionButton = () => {
-  const result = render(
-    <NavigationBar selectNextClass={mockSelectNextClassHandler} />
-  );
+jest.mock("../src/components/shortcut/NextClass", () => ({
+  NextClass: jest.fn(),
+}));
 
-  fireEvent.press(result.container);
-  return result;
-};
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 describe("Testing the Homepages's Navigation Bar", () => {
   test("Should be able to render", async () => {
